3e7b9c6a56c93a3766d4a067c642c741f7a887ed
alu0100825985 <alu0100825985@ull.edu.es>
2016-04-09 20:17:27 +0100

    Añadido el fichero gulpfile.js con las tares de minificación

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+
+//-----> DEPENDENCIAS
+var gulp = require('gulp'),
+	htmlminify = require('gulp-minify-html'),
+	minify = require('gulp-minify'),
+	clean = require('gulp-clean'),
+  uglify = require('gulp-uglify'),
+  cleancss = require('gulp-clean-css');
+
+//-----> CONFIGURACIÓN DE LAS TAREAS
+gulp.task('minify-js', function() {
+  gulp.src(['csv.js', 'main.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('minified/js/'))
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+    .pipe(cleancss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('minified/css'));
+})
+
+gulp.task('minify-html', function() {
+  return gulp.src('*.html')
+    .pipe(htmlminify({collapseWhitespace: true}))
+    .pipe(gulp.dest('minified/html'))
+});
+
+gulp.task('clean', function () {
+	return gulp.src('minified/', {read: false})
+		.pipe(clean());
+});
+
+gulp.task('default', ['minify-js', 'minify-css', 'minify-html'], function() { // Tarea por defecto. Ejecutamos todas las pruebas minify.
+});
