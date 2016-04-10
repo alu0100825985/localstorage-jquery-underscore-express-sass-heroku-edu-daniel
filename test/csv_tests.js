814385b64d5fcc3019fbe92de343d491ec62fb1b
alu0100825985 <alu0100825985@ull.edu.es>
2016-04-10 11:29:21 +0100

    1º Prueba con una cadena de elementos entrecomillados - SUPERADA CON ÉXITO

--- test/csv_tests.js
+++ test/csv_tests.js
@@ -1,6 +1,6 @@
 var expect = chai.expect
 
-describe("CSV", function () {
+describe("PRUEBAS CSV", function () {
   var sandbox;
 
   beforeEach(function() {
@@ -11,5 +11,16 @@ describe("CSV", function () {
 
   afterEach(function() {
     sandbox.restore();
+  });
+
+  describe("Pruebas para la función calculate()", function() {
+    it("Cadena con elementos entrecomillados", function() {
+      var input = '"Esto", "son", "elementos", "entrecomillados"';
+      var fun_cal = calculate(input);
+      expect(fun_cal[0].value[0]).to.equal("Esto");
+      expect(fun_cal[0].value[1]).to.equal("son");
+      expect(fun_cal[0].value[2]).to.equal("elementos");
+      expect(fun_cal[0].value[3]).to.equal("entrecomillados");
+    });
   });
 });
